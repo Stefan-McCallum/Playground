8358d08f53fdfaaa9e63a6fec1db3634e885ac32
anton <akropp@gmail.com>
2013-03-26 10:13:38 -0400

    playground checkin

---
+++ ShowNTell/ShowNTell/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Onoffswitch.NetDuinoUtils.Utils;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
+
+namespace ShowNTell
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+
+            LcdWriter.Instance.Write("BOOM!");
+
+            NetDuinoUtils.KeepRunning();
+        }
+
+    }
+}
