6462fc87009bdb81b89fc7eeeac7a31c06ea12ef
anton <akropp@gmail.com>
2013-05-05 14:17:14 -0400

    adding readme

---
+++ FloodFill/FloodFill/ListUtils.fs
@@ -0,0 +1,10 @@
+﻿module ListUtils
+
+
+(*
+    Finds all items of list2 that are not in list1
+*)
+
+let except list1 list2 = 
+    let listContainsElement item = List.exists (fun i -> i = item) list1
+    List.filter(fun item -> not (listContainsElement item)) list2
