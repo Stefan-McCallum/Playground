bc07c98568b314cb43461f44d20b4b59793e88f1
anton <akropp@gmail.com>
2013-05-30 13:53:22 -0400

    updating naive bayes and adding fogbugz parsing

--- MachineLearning/NaiveBayes/NaiveBayes/VocabBuilder.cs
+++ MachineLearning/NaiveBayes/NaiveBayes/VocabBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using NaiveBayes.Data;
 
 namespace NaiveBayes
@@ -22,14 +23,33 @@ namespace NaiveBayes
             "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
                 .Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries).ToList();
 
-        private static List<string> _punctuation = new List<string>{"!", ".", ",", ";", "\"", "/", "'", ":", "-", "_", "+", "?"}; 
+        private static List<string> _punctuation = new List<string>{"!", ".", ",", ";", "\"", "/", "'", ":", "-", "_", "+", "?", "[", "]", "=", "(", ")", ">", "<"}; 
         private static String StripPunctuation(string input)
         {
             return _punctuation.Aggregate(input, (acc, item) => acc.Replace(item, ""));
         }
+
+        private static String RemoveNumbers(string input)
+        {
+            return Regex.Replace(input, "[0-9]*", "");
+        }
+
         private static String StripStopWords(string input)
         {
             var word = StripPunctuation(input);
+
+            word = RemoveNumbers(word);
+
+            if (String.IsNullOrWhiteSpace(word))
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(word))
+            {
+                return null;
+            }
+
             if (_stopWords.BinarySearch(word) < 0)
             {
                 return word.ToLowerInvariant();
